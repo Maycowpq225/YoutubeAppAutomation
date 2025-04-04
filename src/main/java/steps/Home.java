24dcf1c589ed89b6101a69307ad315dd993514a2
Maycowpq225 <maycowfellipo@hotmail.com>
2025-04-04 20:53:52 +0100

    JUnit 5 and Cucumber added to the project Successfully

---
+++ src/main/java/steps/Home.java
@@ -0,0 +1,12 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class Home {
+
+    @Given("that I open the youtube App")
+    public void acepptCookies() {
+        System.out.println("Olá mundo");
+    }
+
+}
