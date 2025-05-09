165ff71621ec4f16b36e04fbb213059d0d7006a6
Maycowpq225 <maycowfellipo@hotmail.com>
2025-05-08 22:03:31 -0300

    dynamic tag configuration

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,14 @@
 package runner;
 import org.junit.platform.suite.api.*;
-import utils.DriverConfig;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps,hooks")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
-@IncludeTags("Test_004")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Test_004")
+//@IncludeTags("Test_004")
 public class TestRunner {
 }
